c25b2fca47ed2acd0953e4d03bf87ccb5c696177
Code Stack <codestack.net@gmail.com>
2018-10-28 14:47:21 +1100

    Added option to specify extension line for the dimension while setting the direction

--- Base/Extensions/DimensionEx.cs
+++ Base/Extensions/DimensionEx.cs
@@ -17,7 +17,7 @@ namespace SolidWorks.Interop.sldworks
         }
 
         public static void SetDirection(this IDimension dim,
-            Point originPt, Vector dir, double length)
+            Point originPt, Vector dir, double length, Vector extDir = null)
         {
             var dimDirVec = m_MathUtils.CreateVector(dir.ToArray()) as MathVector;
             var startPt = m_MathUtils.CreatePoint(originPt.ToArray()) as IMathPoint;
@@ -30,18 +30,33 @@ namespace SolidWorks.Interop.sldworks
                 m_MathUtils.CreatePoint(new double[3]) as IMathPoint
             };
 
-            MathVector extDirVec = null;
+            //MathVector extDirVec = null;
 
-            var yVec = new Vector(0, 1, 0);
-            if (dir.IsSame(yVec))
+            if (extDir == null)
             {
-                var xVec = new double[] { 1, 0, 0 };
-                extDirVec = m_MathUtils.CreateVector(xVec) as MathVector;
+                var yVec = new Vector(0, 1, 0);
+                if (dir.IsSame(yVec))
+                {
+                    extDir = new Vector(1, 0, 0);
+                }
+                else
+                {
+                    extDir = yVec.Cross(dir);
+                }
             }
-            else
-            {
-                extDirVec = (m_MathUtils.CreateVector(yVec.ToArray()) as MathVector).Cross(dimDirVec) as MathVector;
-            }
+
+            //var yVec = new Vector(0, 1, 0);
+            //if (dir.IsSame(yVec))
+            //{
+            //    var xVec = new double[] { 1, 0, 0 };
+            //    extDirVec = m_MathUtils.CreateVector(xVec) as MathVector;
+            //}
+            //else
+            //{
+            //    extDirVec = (m_MathUtils.CreateVector(yVec.ToArray()) as MathVector).Cross(dimDirVec) as MathVector;
+            //}
+
+            var extDirVec = m_MathUtils.CreateVector(extDir.ToArray()) as MathVector;
             
             dim.DimensionLineDirection = dimDirVec;
             dim.ExtensionLineDirection = extDirVec;
